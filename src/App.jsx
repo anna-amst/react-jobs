78046f7f0a41a578023de79ca81397a9346f5093
Anna Vasileva <vasilieva.a@icloud.com>
2024-07-07 11:39:28 -0400

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,27 @@ import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFound from "./pages/NotFound";
 import JobPage, {jobLoader} from "./pages/JobPage";
+import AddJobPage from "./pages/AddJobPage";
+
+
+
+const App = () => {
+
+const addJob= (newJob) => {
+  
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout />}>
   <Route index element={<HomePage />} />
   <Route path='/jobs' element={<JobsPage />} />
+  <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob}/>} />
   <Route path='/jobs/:id' element={<JobPage />} loader = {jobLoader}/>
   <Route path='*' element={<NotFound />} />
   </Route>)
 );
 
-const App = () => {
   return <RouterProvider router={router} />;
 };
 
